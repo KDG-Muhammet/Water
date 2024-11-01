e9062ef2d51de693d76a850ea71b6d89bd00c4ce
muhammet murat <muhammet.murat@student.kdg.be>
2024-11-01 19:09:50 +0100

    water - code cleand up + banner change

--- src/main/java/be/kdg/sa/repository/ShipRepository.java
+++ src/main/java/be/kdg/sa/repository/ShipRepository.java
@@ -3,11 +3,9 @@ package be.kdg.sa.repository;
 import be.kdg.sa.domain.Ship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ShipRepository extends JpaRepository<Ship, Integer> {
 
-    Optional<Ship> findShipByVesselNumber(String vesselNumber);
     Ship getShipByVesselNumber(String vesselNumber);
 
 }
