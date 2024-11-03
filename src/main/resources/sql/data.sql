a4ef0730b300a113e774345fb95de9796841ce60
muhammet murat <muhammet.murat@student.kdg.be>
2024-11-03 14:26:59 +0100

    water - extra data

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -3,3 +3,6 @@ VALUES(gen_random_uuid() , '2024-10-01 12:00:00','ship name', 'test_vessel_numbe
 
 insert into shipping_order(referenceuuid, po_number)
 VALUES(gen_random_uuid() , 'testpoNumber' );
+
+insert into shipping_order(referenceuuid, po_number)
+VALUES(gen_random_uuid() , 'testpoNumber2' );
